2c123574bb99de0af057c1e1f10b1cc3f77e9b2e
Sonu Nigam <drop@outlook.in>
2021-12-26 23:37:54 +0530

    removed BASE PATH Variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
